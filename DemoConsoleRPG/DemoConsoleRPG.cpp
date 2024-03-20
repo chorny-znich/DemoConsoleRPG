b385ae64fa9ac3bdfb45b8e3a905b9b7f433e9fe
chorny-znich <vargar.dt@gmail.com>
2024-03-20 10:58:32 +0300

    feat: change the version number

--- DemoConsoleRPG/DemoConsoleRPG.cpp
+++ DemoConsoleRPG/DemoConsoleRPG.cpp
@@ -1,5 +1,5 @@
 // the demo version of the RPG rendering in the windows console
-// version 0.0.1
+// version 0.0.2
 
 #include "engine.h"
 
--- DemoConsoleRPG/npc.cpp
+++ DemoConsoleRPG/npc.cpp
@@ -1,3 +1,4 @@
+#include <string>
 #include <format>
 #include "npc.h"
 
